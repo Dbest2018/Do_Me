e6bfbe79020a9c30902239aa018a1e314f918206
Dam <shit2wice@gmail.com>
2022-08-25 18:13:47 +0100

    feat: Add Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./Nav.css";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      <div className="nav__all">All</div>
+      <div className="nav__active">Active</div>
+      <div className="nav__completed">Completed</div>
+    </div>
+  );
 };
 
 export default Nav;
