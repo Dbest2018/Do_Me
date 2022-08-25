802afbeb5b0c5950dde0b8788c67dd239abb085d
Dam <shit2wice@gmail.com>
2022-08-25 17:59:27 +0100

    feat: Add todo list

--- src/components/Do/Do.js
+++ src/components/Do/Do.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Do.css";
+import close from "../../images/icon-cross.svg";
 
 const Do = () => {
-  return <div>Do</div>;
+  return (
+    <div className="do">
+      <input className="do__radio" type="radio" />
+      <input
+        className="do__text"
+        type="text"
+        placeholder="Create a new todo..."
+      />
+      <img className="do__close" src={close} alt="close icon" />
+    </div>
+  );
 };
 
 export default Do;
--- src/components/DoList/DoList.js
+++ src/components/DoList/DoList.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./DoList.css";
+import Do from "../Do/Do";
 
 const DoList = () => {
-  return <div>DoList</div>;
+  return (
+    <div className="doList">
+      {[0, 1, 2, 3, 4, 5, 6].map((num) => (
+        <Do key={num} />
+      ))}
+    </div>
+  );
 };
 
 export default DoList;
