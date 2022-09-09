0a5c3d6cb5a2c077488e648bb9d7a8d3f29cce97
Dam <shit2wice@gmail.com>
2022-09-09 23:05:56 +0100

    refactor: Remove debugging logs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   useEffect(() => {
     let active = false;
     const fetchTodos = async () => {
-      console.log("rendering");
       const querySnapshot = await getDocs(collection(db, "todo-items"));
       console.log(querySnapshot);
       querySnapshot.forEach((doc) => {
